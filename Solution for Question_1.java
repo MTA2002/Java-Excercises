5caa3e0d5f18177b12affbaebb01d57e5257433f
MTA2002 <104497136+MTA2002@users.noreply.github.com>
2022-12-11 13:17:17 +0300

    Update and rename Question_1.java to Solution for Question_1.java

--- Solution for Question_1.java
+++ Solution for Question_1.java
@@ -1,3 +1,8 @@
+/*
+Question:Write a program that displays all the numbers from 100 to 200, ten per line, that are 
+divisible by 5 or 6, but not both. Numbers are separated by exactly one space.
+*/
+//Solution
 public class Main {
     public static void main(String[] args) {
       int number;
